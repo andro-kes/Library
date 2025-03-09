5eb889cc777778536d7b82e86e9aab5e18d8cce2
Andrey Denisov <and.denisov26@gmail.com>
2025-03-09 15:10:40 +0300

    Modified db file

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,7 +1,64 @@
 package database
 
-import "fmt"
+import (
+	"encoding/csv"
+	"io"
+	"os"
+	"log"
+)
+
+type db = map[int]Record
+
+type Record struct {
+	Title string
+	Author string
+	Genre string
+	Height string
+	Publisher string
+}
 
 func CreateDataBase(){
-	fmt.Println("GO")
+	id := 0
+	booksBase := make(db)
+	reader := createCSVReader()
+
+	_, err := reader.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		record, err := reader.Read()
+
+		if err == io.EOF {
+			log.Println("База построена")
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		booksBase[id] = createRecord(record)
+		id++
+	}
+}
+
+func createCSVReader() *csv.Reader {
+	file, err := os.Open("books.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	return reader
+}
+
+func createRecord(record []string) Record {
+	return Record{
+		Title: record[0],
+		Author: record[1],
+		Genre: record[2],
+		Height: record[3],
+		Publisher: record[4],
+	}
 }
